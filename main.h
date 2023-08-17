b5dbd191dac407aa20d6b07487e9f423f69006d5
Monwabisindlovu <monwabisindlovu78@gmail.com>
2023-08-17 00:28:22 -0700

    betty.c

--- main.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-void print_prompt(void);
-char *read_line(void);
-
-#endif
--- shell.h
+++ shell.h
@@ -10,22 +10,15 @@
 #include <sys/wait.h>
 
 
+#define BUFFER_SIZE 1024
+
 int print_hello(void);
-int factorial(int n);
-int main(void);
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
-char **tokenize_input(char *input);
-int execute_command(char **args);
-void free_tokens(char **tokens);
-char *_getenv(char **env);
-int _values_path(char **arg, char **env);
-int _strcmp(char *s1, char *s2);
-char *_strcpy(char *dest, char *src);
-char *_strcat(char *dest, char *src);
-char *_get_path(char **env);
-char *_getline_command(void);
-char **tokenize(char *lineptr);
-int _fork_fun(char **arg, char **av, char **env, char *lineptr, int np, int c);
+void print_prompt(void);
+char *read_line(void);
+char **lsh_split_line(char *line);
+int lsh_cd(char **args);
+int lsh_clear(void);
+
 
 #endif /* SHELL_H */
 
