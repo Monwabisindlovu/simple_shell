05bcc80cac893d4719515cf7feb77c244da61e62
Wandzfuture <michael.zabas@gmail.com>
2023-08-23 01:06:51 -0700

    shell.c

--- read_line.c
+++ read_line.c
@@ -13,31 +13,30 @@
  */
 char *read_line(void)
 {
-char *line = NULL;
-size_t buffer_size = 0;
-size_t len;
+	char *line = NULL;
+	size_t buffer_size = 0;
+	size_t len;
 
-if (getline(&line, &buffer_size, stdin) == -1)
-{
-if (feof(stdin))
-{
-free(line);
-return (NULL);
-}
-else
-{
-perror("read_line");
-free(line);
-exit(EXIT_FAILURE);
-}
-}
+	if (getline(&line, &buffer_size, stdin) == -1)
+	{
+		if (feof(stdin))
+		{
+			free(line);
+			return (NULL);
+		}
+		else
+		{
+			perror("read_line");
+			free(line);
+			exit(EXIT_FAILURE);
+		}
+	}
 
-len = strlen(line);
-if (len > 0 && line[len - 1] == '\n')
-{
-line[len - 1] = '\0';
-}
+	len = strlen(line);
+	if (len > 0 && line[len - 1] == '\n')
+	{
+		line[len - 1] = '\0';
+	}
 
-return (line);
+	return (line);
 }
-
--- split_line.c
+++ split_line.c
@@ -10,40 +10,23 @@
 char **split_line(char *line)
 {
 	size_t token_count = 0;
-	size_t buffer_size = BUFSIZE;
-	char **tokens = malloc(sizeof(char *) * buffer_size);
+	char **tokens = malloc(sizeof(char*));
 	char *token = strtok(line, TOKEN_DELIMITERS);
 
-	if (tokens == NULL)
+	if (token == NULL)
 	{
-		perror("split_line");
-		exit(EXIT_FAILURE);
+		return NULL;
 	}
 
 	while (token != NULL)
 	{
-		tokens[token_count] = strdup(token);
-
-		if (tokens[token_count] == NULL)
-		{
-			perror("split_line");
-			exit(EXIT_FAILURE);
-		}
-
+		tokens[token_count] = token;
 		token_count++;
 
-		if (token_count >= buffer_size)
-		{
-			buffer_size += BUFSIZE;
-			tokens = realloc(tokens, sizeof(char *) * buffer_size);
-			if (tokens == NULL)
-			{
-				perror("split_line");
-				exit(EXIT_FAILURE);
-			}
-		}
+		tokens = realloc(tokens, (token_count + 1) *sizeof(char*));
 		token = strtok(NULL, TOKEN_DELIMITERS);
 	}
+
 	tokens[token_count] = NULL;
-	return (tokens);
+	return tokens;
 }
